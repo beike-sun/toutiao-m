d3bb86f54f2e228bb403c383af0e651a45c489dd
songlihong <1876766912@qq.com>
2020-05-09 10:59:38 +0800

    设置rem基准值

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import router from './router'
 import store from './store'
 import Vant from 'vant'
 import 'vant/lib/index.css'
+// 自动设置rem基准值（html标签字体大小）
+import 'amfe-flexible'
 Vue.use(Vant)
 Vue.config.productionTip = false
 new Vue({
