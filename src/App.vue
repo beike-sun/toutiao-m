40f7c0409cc11662ffc4c628ba9170c844ec45bc
songlihong <1876766912@qq.com>
2020-05-09 15:19:25 +0800

    路由页面配置完成

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <template>
   <div id="app">
-   <h1>移动端黑马头条</h1>
-   <!-- 字体图标应用 -->
-   <i class="iconfont iconpinglun" ></i>
    <!-- 路由出口 -->
     <router-view/>
   </div>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,39 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
-
+// 路由表
 const routes = [
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('@/views/login')
+  },
+  {
+    path: '/',
+    component: () => import('@/views/layout'),
+    children: [
+      {
+        path: '',
+        name: 'home',
+        component: () => import('@/views/home')
+      },
+      {
+        path: '/qa',
+        name: 'qa',
+        component: () => import('@/views/qa')
+      },
+      {
+        path: '/my',
+        name: 'my',
+        component: () => import('@/views/my')
+      },
+      {
+        path: '/video',
+        name: 'video',
+        component: () => import('@/views/video')
+      }
+    ]
+  }
 ]
 
 const router = new VueRouter({
