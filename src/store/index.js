e63b06105350c6599a96c6d757c915b240f47f45
songlihong <1876766912@qq.com>
2020-05-11 00:10:09 +0800

    优化分装本地存储模块

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,19 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import {
+  getItem,
+  setItem
+} from '@/utlis/storage.js'
 Vue.use(Vuex)
-
+const USER_KEY = 'toutiao-user'
 export default new Vuex.Store({
   state: {
-    user: JSON.parse(window.localStorage.getItem('user'))
+    user: getItem(USER_KEY)
   },
   mutations: {
     setUser (state, data) {
       state.user = data
-      window.localStorage.setItem('user', JSON.stringify(state.user))
+      setItem(USER_KEY, state.user)
     }
   },
   actions: {
---
+++ src/utlis/storage.js
@@ -0,0 +1,18 @@
+// 封装本地存储模块
+export const getItem = name => {
+  const data = window.localStorage.getItem(name)
+  try {
+    return JSON.parse(data)
+  } catch (err) {
+    return data
+  }
+}
+export const setItem = (name, value) => {
+  if (typeof value === 'object') {
+    value = JSON.stringify(value)
+  }
+  return window.localStorage.setItem(name, value)
+}
+export const removeItem = name => {
+  return window.localStorage.removeItem(name)
+}
