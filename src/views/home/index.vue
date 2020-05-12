2c07620ea593916e82f57fbc8764ac3949a1bb22
songlihong <1876766912@qq.com>
2020-05-13 07:39:27 +0800

    完成首页-导航栏部分

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,16 @@
 <template>
-  <div class="home-container">首页</div>
+  <div class="home-container">
+    <!-- 导航栏 -->
+  <van-nav-bar class="nav-container">
+ <van-button
+   slot="title"
+   round
+   type="info"
+   icon="search"
+   class="search-box"
+   >搜索</van-button>
+</van-nav-bar>
+  </div>
 </template>
 
 <script>
@@ -8,6 +19,25 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="less">
+/* 导航栏样式 */
+.nav-container{
+  height: 48px;
+  background-color: #3296fa;
+   .van-nav-bar__title {
+    max-width: none;
+  }
+  .search-box{
+    height: 34px;
+    width: 227px;
+    background-color: #5babfb;
+  }
+  .van-icon{
+    font-size: 16px;
+    color: #fff;
+  }
+  .van-button_text{
+    font-size: 14px;
+  }
+}
 </style>
