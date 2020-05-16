2b17bcf2a2e2c9c94fc8e00e2a6632fe9a3d37f5
songlihong <1876766912@qq.com>
2020-05-16 23:26:45 +0800

    完成频道布局样式，但是未清除cell默认边框

--- src/views/home/compontents/channel-edit.vue
+++ src/views/home/compontents/channel-edit.vue
@@ -1,28 +1,43 @@
 <template>
   <div class="channel-edit">
     <van-cell-group>
-  <van-cell title="单元格" value="内容" center  >
-  <div slot="title">我的频道</div>
+  <van-cell center >
+  <div
+   slot="title"
+   class="channel-title"
+   >我的频道</div>
   <div>
     <van-button
       plain
       type="danger"
       round
-      size="small"
+      size="mini"
  >编辑</van-button>
   </div>
   </van-cell>
 </van-cell-group>
-<van-grid :gutter="10">
-  <van-grid-item v-for="value in 8" :key="value"  text="文字" />
+<van-grid :gutter="10" >
+  <van-grid-item
+   class="grid-item"
+   v-for="value in 8"
+   :key="value"
+   text="文字"
+       />
 </van-grid>
 <van-cell-group>
   <van-cell>
-    <div slot="title">频道推荐</div>
+    <div
+     slot="title"
+    class="channel-title"
+     >频道推荐</div>
   </van-cell>
 </van-cell-group>
 <van-grid :gutter="10">
-  <van-grid-item v-for="value in 8" :key="value"  text="文字" />
+  <van-grid-item
+    class="grid-item"
+    v-for="value in 8"
+    :key="value"
+    text="文字" />
 </van-grid>
   </div>
 </template>
@@ -36,5 +51,23 @@ export default {
 <style scoped  lang="less">
 .channel-edit{
   padding-top: 54px;
+  .channel-title{
+    font-size: 16px;
+    color: #333;
+  }
+}
+.grid-item{
+  width: 80px;
+  height: 43px;
+}
+/deep/.van-grid-item__content{
+background-color: #f4f5f6;
+}
+/deep/.van-grid-item__text{
+  color: #222;
+}
+.van-grid{
+  margin-top: -24px;
+  margin-bottom: 38px;
 }
 </style>
