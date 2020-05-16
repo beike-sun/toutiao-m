d459cf47ea3403810221fbdc6723d213b68a9b10
songlihong <1876766912@qq.com>
2020-05-16 22:43:36 +0800

    编辑页面结构搭建完成

--- src/views/home/compontents/channel-edit.vue
+++ src/views/home/compontents/channel-edit.vue
@@ -1,5 +1,30 @@
 <template>
-  <div class="channel-edit">频道编辑</div>
+  <div class="channel-edit">
+    <van-cell-group>
+  <van-cell title="单元格" value="内容" center  >
+  <div slot="title">我的频道</div>
+  <div>
+    <van-button
+      plain
+      type="danger"
+      round
+      size="small"
+ >编辑</van-button>
+  </div>
+  </van-cell>
+</van-cell-group>
+<van-grid :gutter="10">
+  <van-grid-item v-for="value in 8" :key="value"  text="文字" />
+</van-grid>
+<van-cell-group>
+  <van-cell>
+    <div slot="title">频道推荐</div>
+  </van-cell>
+</van-cell-group>
+<van-grid :gutter="10">
+  <van-grid-item v-for="value in 8" :key="value"  text="文字" />
+</van-grid>
+  </div>
 </template>
 
 <script>
@@ -8,6 +33,8 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped  lang="less">
+.channel-edit{
+  padding-top: 54px;
+}
 </style>
