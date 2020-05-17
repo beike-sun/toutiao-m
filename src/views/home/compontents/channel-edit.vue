83e333899ba8a541b91b13119c1658fff576a999
songlihong <1876766912@qq.com>
2020-05-17 12:44:10 +0800

    完成编辑的处理状态及删除频道

--- src/views/home/compontents/channel-edit.vue
+++ src/views/home/compontents/channel-edit.vue
@@ -12,16 +12,19 @@
       type="danger"
       round
       size="mini"
- >编辑</van-button>
+      @click="isEdit=!isEdit"
+ >{{isEdit? '完成': '编辑'}}</van-button>
   </div>
   </van-cell>
 </van-cell-group>
 <van-grid :gutter="10" >
   <van-grid-item
    class="grid-item"
+   :icon="(isEdit && index !== 0) ? 'clear' : ''"
    v-for="(channel, index) in userChannels"
    :key="index"
    :text="channel.name"
+   @click="onUserChannelClick(index)"
        />
 </van-grid>
 <van-cell-group>
@@ -56,7 +59,8 @@ export default {
   },
   data () {
     return {
-      allChannels: []
+      allChannels: [],
+      isEdit: false
     }
   },
   computed: {
@@ -80,6 +84,21 @@ export default {
     },
     onAdd (channel) {
       this.userChannels.push(channel)
+    },
+    onUserChannelClick (index) {
+      if (this.isEdit && index !== 0) {
+        // 点击的时候，如果是编辑状态就删除频道index
+        this.deleteChannel(index)
+      } else {
+        //  如果是非编辑状态就切换频道
+        this.switchChannel(index)
+      }
+    },
+    deleteChannel (index) {
+      this.userChannels.splice(index, 1)
+    },
+    switchChannel (index) {
+      console.log('切换频道')
     }
   }
 }
@@ -106,5 +125,15 @@ background-color: #f4f5f6;
 .van-grid{
   margin-top: -24px;
   margin-bottom: 38px;
+}
+/deep/.van-grid-item__icon {
+    font-size: 20px;
+    position: absolute;
+    top: -9px;
+    right: -7px;
+    font-size: 20px;
+}
+ .van-grid-item__content {
+    position: relative;
 }
 </style>
