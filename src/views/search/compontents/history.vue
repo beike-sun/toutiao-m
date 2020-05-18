8e79330518b09c64fff1d646b97d8dae087d2fd3
songlihong <1876766912@qq.com>
2020-05-18 18:44:20 +0800

    完成历史记录的删除显示状态

--- src/views/search/compontents/history.vue
+++ src/views/search/compontents/history.vue
@@ -1,10 +1,15 @@
 <template>
   <div id="history-container">
   <van-cell title="历史记录" value="内容" >
-      <div>
+      <div  v-if="isDeleteShow">
           <span>全部删除   </span>
-          <span>完成</span>
+          <span @click="isDeleteShow=false">完成</span>
       </div>
+      <van-icon
+       name="delete"
+        v-else
+        @click="isDeleteShow=true"
+         />
   </van-cell>
     <van-cell
      :title="history"
@@ -12,7 +17,10 @@
       v-for="(history, index) in searchHistories"
       :key="index"
        >
-     <van-icon name="close" />
+     <van-icon
+      name="close"
+      v-show="isDeleteShow"
+       />
     </van-cell>
   </div>
 </template>
@@ -24,6 +32,11 @@ export default {
     searchHistories: {
       type: Array,
       required: true
+    }
+  },
+  data () {
+    return {
+      isDeleteShow: false
     }
   }
 }
