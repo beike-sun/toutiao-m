e8221fdabec91a65215c3e3021482d706a7daf4a
songlihong <1876766912@qq.com>
2020-05-21 16:17:06 +0800

    完成用户编辑页面布局

--- src/views/user-profile/index.vue
+++ src/views/user-profile/index.vue
@@ -1,9 +1,22 @@
 <template>
   <div class="user-profile">
     <van-nav-bar
-  title="标题"
-  left-arrow
+    class="userNewsTitle"
+    title="个人信息"
+    left-arrow
 />
+<van-cell title="头像" is-link center>
+  <van-image
+  width="30px"
+  height="30px"
+  round
+  fit="cover"
+  src="https://img.yzcdn.cn/vant/cat.jpeg"
+/>
+</van-cell>
+<van-cell title="昵称" is-link  value="帅哥一枚"/>
+<van-cell title="性别" is-link  value="男"/>
+<van-cell title="生日" is-link />
   </div>
 </template>
 
@@ -14,5 +27,16 @@ export default {
 </script>
 
 <style>
-
+html{
+  background: #f4f5f6;
+}
+.userNewsTitle{
+  background-color: #3296fa;
+}
+.van-nav-bar__title{
+  color: #fff;
+}
+.van-icon-arrow-left::before{
+  color: #fff;
+}
 </style>
